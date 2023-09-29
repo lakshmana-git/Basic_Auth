36f663e03a9c34e463c5e1e79938188441bc13b6
lakshmana-git <devlakshmanarao@gmail.com>
2023-09-29 16:03:37 +0530

    connected to db

--- backend/index.js
+++ backend/index.js
@@ -1,5 +1,13 @@
 import express from "express";
+import mongoose  from "mongoose";
+import dotenv from 'dotenv'
+dotenv.config();
 
+mongoose.connect(process.env.DB_URL).then(()=>{
+    console.log("Connected to DB......")
+}).catch((error) => {
+    console.error("Error connecting to DB:", error);
+  });
 const app = express()
 
 
